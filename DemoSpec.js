bfdb8235390e593098db6c6cab5591a82dd1a5ba
Sarmistha_Jena <sarmistha.jena@gmail.com>
2020-12-31 16:40:38 +0530

    Added reporter

--- DemoSpec.js
+++ DemoSpec.js
@@ -3,6 +3,7 @@ var logger = require('./log');
 describe('Bank Home page Test', () => {
     it('verify the page title', () => {
         browser.get('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
+        browser.sleep(3000);
         expect(element(by.buttonText('Customer Login')).isDisplayed()).toBe(true);
         expect(element(by.buttonText('Bank Manager Login')).isDisplayed()).toBe(true, "Bank Manager Login button expected to exist.");
         browser.getTitle().then((title) => {
--- conf.js
+++ conf.js
@@ -1,5 +1,11 @@
+var HTMLReport = require('protractor-html-reporter-2');
+var jasmineReporters = require('jasmine-reporters');
+
+require('jasmine2-protractor-utils');
+
 exports.config = {
-    directConnect: true,
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+  directConnect: true,
   capabilities: {
 
     'browserName': 'chrome',
@@ -7,4 +13,54 @@ exports.config = {
 
   },
   specs: ['DemoSpec.js'],
+
+  plugins: [{
+    package: 'jasmine2-protractor-utils',
+    disableHTMLReport: true,
+    disableScreenshot: false,
+    screenshotPath:'./screenshots',
+    screenshotOnExpectFailure:false,
+    screenshotOnSpecFailure:true,
+    clearFoldersBeforeTest: true
+  }],
+
+  onPrepare: function () {
+
+    browser.ignoreSynchronization = true;
+    browser.driver.manage().window().maximize();
+
+    //
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+      consolidateAll: true,
+      savePath: './',
+      filePrefix: 'xmlresults'
+    }));
+  },
+
+  onComplete: function () {
+    var browserName, browserVersion;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+      browserName = caps.get('browserName');
+      browserVersion = caps.get('version');
+      platform = caps.get('platform');
+
+      var HTMLReport = require('protractor-html-reporter-2');
+
+      testConfig = {
+        reportTitle: 'Protractor Test Execution Report',
+        outputPath: './',
+        outputFilename: 'ProtractorTestReport',
+        screenshotPath: './screenshots',
+        testBrowser: browserName,
+        browserVersion: browserVersion,
+        modifiedSuiteName: false,
+        screenshotsOnlyOnFailure: true,
+        testPlatform: platform
+      };
+      new HTMLReport().from('xmlresults.xml', testConfig);
+    });
+  }
+
 }
